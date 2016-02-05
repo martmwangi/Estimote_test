f19ea74cef0a83231eb234e25516665fdca4ba96
martmwangi <luthermwangi2@gmail.com>
2016-02-06 01:44:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hulk/estimote/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hulk.estimote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.estimote.sdk.SystemRequirementsChecker;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+    }
+
+}
